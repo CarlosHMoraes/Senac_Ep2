d9356fed692ed686ec7155f160a2211765e74b56
CarlosHMoraes <36535938+CarlosHMoraes@users.noreply.github.com>
2019-11-18 22:32:10 -0300

    Update taxi3_ep3.py

--- taxi3_ep3.py
+++ taxi3_ep3.py
@@ -4,7 +4,8 @@ from mrjob.step import MRStep
 class TaxiNYC(MRJob):
     def steps(self):
         return [
-            MRStep(mapper=self.busca_valor, combiner=self.soma_valor, reducer=self.media_final)
+            MRStep(mapper=self.busca_valor, combiner=self.soma_valor, reducer=self.media_final),
+            MRStep(reducer=self.media_final_2)
         ]
     
     def busca_valor(self, _, line):
@@ -13,15 +14,26 @@ class TaxiNYC(MRJob):
             yield colunas[1][0:10], float(colunas[16])
 
     def soma_valor(self, key, values):
-        yield count(key), sum(values)
+        yield key, sum(values)
             
-    def media15min(self, dias, total_valor):
-        minutos_media = ((dias * 24 * 60) / 15)
-        media = total_valor / minutos_media
-        return None, media            
+    def media15min(self, dias, valores):
+        total_valor = 0
+        for valor in valores: 
+            total_valor += valor / ((24 * 60) / 15)
+        return None, total_valor            
             
     def media_final(self, key, values):
-        yield self.media15min(key, sum(values))
+        yield self.media15min(key, values)
+
+    def media(self, dias, valores):
+        count, total_valor = 0, 0
+        for valor in valores: 
+            total_valor+= valor
+            count+=1
+        return None, (total_valor/count)  
+
+    def media_final_2(self, key, values):
+        yield self.media(key, values)
 
 if __name__ == '__main__':
     TaxiNYC.run()
